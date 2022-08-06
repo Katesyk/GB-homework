037c1d821d231fcaf75619baf05b791b16da8c7a
Stacy <anastasia.grechina@gmail.com>
2022-08-06 22:45:01 +0300

    completed n5

---
+++ les_1_n_5.py
@@ -0,0 +1,7 @@
+a = int(input('Введите прибыль вашей фирмы: '))
+b = int(input('Введите издержки вашей фирмы: '))
+bablishko = a - b
+if bablishko > 0:
+    print('Вы прекрасны!')
+if bablishko < 0:
+    print('Кажется, нужно кого-то наругать')
